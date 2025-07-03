f666c240ab17a21d311cf668d508b351c781e410
eastdh <eastdh@gmail.com>
2025-07-03 15:09:23 +0900

    initial commit

---
+++ src/main/java/io/github/restart/gmo_danggeun/GmoDanggeunApplication.java
@@ -0,0 +1,13 @@
+package io.github.restart.gmo_danggeun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmoDanggeunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GmoDanggeunApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/restart/gmo_danggeun/GmoDanggeunApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.restart.gmo_danggeun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmoDanggeunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
