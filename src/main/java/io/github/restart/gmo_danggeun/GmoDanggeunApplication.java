afe7042f07a1df0ea0840c996efb6f17de2c0d22
ase10git <ase1031@naver.com>
2025-07-08 14:28:31 +0900

    feat: trade 관련 페이지의 HTML 구조 추가

--- src/main/java/io/github/restart/gmo_danggeun/GmoDanggeunApplication.java
+++ src/main/java/io/github/restart/gmo_danggeun/GmoDanggeunApplication.java
@@ -2,8 +2,10 @@ package io.github.restart.gmo_danggeun;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+// TODO : 프론트 완료 후 exclude 제거
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class GmoDanggeunApplication {
 
 	public static void main(String[] args) {
